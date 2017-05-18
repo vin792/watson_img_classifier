0187b2f2f4de4e37c1b670ecbbd991f7cc664330
Jordan-Weatherford <jrw821@gmail.com>
2017-05-18 15:06:58 -0700

    capture image code written

--- HotdogNotHotdog/ViewController.swift
+++ HotdogNotHotdog/ViewController.swift
@@ -10,13 +10,15 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCapturePhotoCaptureDelegate {
     
+    @IBOutlet weak var tempImageView: UIImageView!
     @IBOutlet weak var cameraView: UIView!
     
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCapturePhotoOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
+    var image : UIImage?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -28,8 +30,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewWillAppear(animated)
         captureSession = AVCaptureSession()
         
-        
-//   might have to change to "high"
+    
         captureSession?.sessionPreset = AVCaptureSessionPresetHigh
         
         let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
@@ -45,9 +46,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if backCameraError == nil && (captureSession?.canAddInput(input))! {
             captureSession?.addInput(input)
             stillImageOutput = AVCapturePhotoOutput()
-// outputSettings no working!!
-//            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
-            
+
             if (captureSession?.canAddOutput(stillImageOutput))! {
                 captureSession?.addOutput(stillImageOutput)
                 previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
@@ -59,11 +58,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    
-    
-    
-    
-    
+    func didPressTakePhoto() {
+        if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+            let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecJPEG])
+            stillImageOutput?.capturePhoto(with: photoSettings, delegate: self)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,5 +73,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    //AVPhotoCapture Delegate function
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        
+        if let sampleBuffer = photoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: nil){
+            image = UIImage(data: dataImage)
+        } else {
+            print("FAILED AT IMAGE PROCESSING")
+        }
+        
+    }
+    
+    @IBAction func captureButtonPressed(_ sender: UIButton) {
+        didPressTakePhoto()
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
