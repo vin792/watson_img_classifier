71a97ac9b3fbcfc067d85e437cd3fb6d85c19abc
Jordan-Weatherford <jrw821@gmail.com>
2017-05-18 15:10:06 -0700

    added new image captured view controller

---
+++ ImageCapturedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ImageCapturedViewController.swift
+//  HotdogNotHotdog
+//
+//  Created by Jordan Russell Weatherford on 5/18/17.
+//  Copyright © 2017 Nicole Zurita. All rights reserved.
+//
+
+import UIKit
+
+class ImageCapturedViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
