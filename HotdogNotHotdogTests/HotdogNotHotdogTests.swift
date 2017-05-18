f27d3914c8b85ce642de079f44333910c2ebacc5
Nicole Zurita <npz2dw@gmail.com>
2017-05-18 11:09:38 -0700

    Initial Commit

---
+++ HotdogNotHotdogTests/HotdogNotHotdogTests.swift
@@ -0,0 +1,36 @@
+//
+//  HotdogNotHotdogTests.swift
+//  HotdogNotHotdogTests
+//
+//  Created by Nicole Zurita on 5/18/17.
+//  Copyright © 2017 Nicole Zurita. All rights reserved.
+//
+
+import XCTest
+@testable import HotdogNotHotdog
+
+class HotdogNotHotdogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
